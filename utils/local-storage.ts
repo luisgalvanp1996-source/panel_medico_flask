c87d6b81e17808a3dbbefc2db8cdd8ca7ba3a677
Juan José Eusebio <82086484+AresNative@users.noreply.github.com>
2025-10-11 12:49:37 -0700

    Create local-storage.ts

---
+++ utils/local-storage.ts
@@ -0,0 +1,40 @@
+export const setLocalStorageItem = (key: string, value: any) => {
+  try {
+    if (typeof window !== "undefined" && window.localStorage) {
+      localStorage.setItem(key, JSON.stringify(value));
+    }
+  } catch (error) {
+    console.log(`Error setting ${key} in localStorage:`, error);
+  }
+};
+
+export const getLocalStorageItem = (key: string) => {
+  try {
+    if (typeof window !== "undefined" && window.localStorage) {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : null;
+    }
+  } catch (error) {
+    console.log("Error while getting item from localStorage:", error);
+  }
+};
+
+export const removeFromLocalStorage = (key: string) => {
+  try {
+    if (typeof window !== "undefined" && window.localStorage) {
+      localStorage.removeItem(key);
+    }
+  } catch (error) {
+    console.log(`Error removing ${key} from localStorage:`, error);
+  }
+};
+
+export const clearLocalStorage = () => {
+  try {
+    if (typeof window !== "undefined" && window.localStorage) {
+      localStorage.clear();
+    }
+  } catch (error) {
+    console.log(`Clear all localStorage:`, error);
+  }
+};
